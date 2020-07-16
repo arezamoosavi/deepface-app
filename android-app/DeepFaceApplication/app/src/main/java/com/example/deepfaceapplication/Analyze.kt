8f275fd778ee54ceecc5cadb3aa5db86e686c018
MahsaSeifikar <m.seifikar93@gmail.com>
2020-07-16 21:11:24 +0430

    Add model for analyze api

---
+++ android-app/DeepFaceApplication/app/src/main/java/com/example/deepfaceapplication/Analyze.kt
@@ -0,0 +1,4 @@
+package com.example.deepfaceapplication
+
+interface Analyze {
+}
---
+++ android-app/DeepFaceApplication/app/src/main/java/models/Emotion.kt
@@ -0,0 +1,29 @@
+package models
+
+import com.google.gson.annotations.SerializedName
+
+class Emotion {
+
+    @field:SerializedName("angry")
+    val angry: Double? = null,
+
+    @field:SerializedName("disgust")
+    val disgust: Double? = null,
+
+    @field:SerializedName("fear")
+    val fear: Double? = null,
+
+    @field:SerializedName("happy")
+    val happy: Double? = null,
+
+    @field:SerializedName("sad")
+    val sad: Double? = null,
+
+    @field:SerializedName("surprise")
+    val surprise: Double = null,
+
+
+    @field:SerializedName("neutral")
+    val neutral: Double? = null
+
+}
