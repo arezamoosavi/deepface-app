b37f497ae4b8ff844c9066c4f608eeae8080d7df
alireza <sdamoosavi@gmail.com>
2020-09-13 00:07:32 +0430

    docker finalized3

--- heroku/run_web.sh
+++ heroku/run_web.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o nounset
 
 
-# RUN python3 preload.py
+python3 preload.py
 
 uvicorn run_app:app --host "0.0.0.0" --port $PORT --reload --ws 'auto' \
 --loop 'auto' --workers 2
