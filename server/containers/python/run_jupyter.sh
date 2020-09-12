8e2128401c12b7a5592d2ea80046418f299e767c
alireza <sdamoosavi@gmail.com>
2020-09-12 22:21:46 +0430

    heroku deepface app updated!

--- server/containers/python/run_jupyter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-sleep 10
-jupyter lab --port=8888 --no-browser --ip=0.0.0.0 --allow-root
--- server/main/client_script.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-url = "http://localhost:8000/analyze"
-my_img = {"file": open("image.jpeg", "rb")}
-r = requests.post(url, files=my_img)
-
-# convert server response into JSON format.
-print(r.text)
